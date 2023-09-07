988d20efa70d7fe0bcdd382eb3fb08027539e538
JPValues <144186729+JPValues@users.noreply.github.com>
2023-09-07 13:36:14 +0900

    完成品

---
+++ CNValues-JP/info/ideologies.js
@@ -0,0 +1,272 @@
+ideologies = [
+    {
+        "name": "無政府共産主義",
+        "stats": {
+            "econ": 100,
+            "govt": 100,
+            "scty": 90
+        },
+        "desc": "無政府共産主義（Anarcho-Communism）は無政府主義の最大派閥で、国家機関と私有制の廃止を主張し、自由意志と互助の基礎の上にそれぞれが能力に応じて働き需要に応じて分配する社会を望んでいる。<br>主要な人物：ピョートル・クロポトキン、エマ・ゴールドマン、ネストル・マフノ",
+        "link": "https://en.wikipedia.org/wiki/Anarcho-communism"
+    },
+    {
+        "name": "正統派マルクス主義",
+        "stats": {
+            "econ": 100,
+            "govt": 70,
+            "scty": 90
+        },
+        "desc": "正統派マルクス主義（Orthodox Marxism）は社会主義革命の必要性を堅持しつつ社会主義民主主義と政治的理由の必要性を強調し改良主義と官僚的民主集中制に反対している。<br>主要な人物：カール・カウツキー、ローザ・ルクセンブルク、アントニオ・グラムシ、ルイ・ピエール・アルチュセール、スラヴォイ・ジゼク",
+        "link": "https://en.wikipedia.org/wiki/Orthodox_Marxism"
+    },
+    {
+        "name": "トロツキー主義",
+        "stats": {
+            "econ": 100,
+            "govt": 50,
+            "scty": 90
+        },
+        "desc": "トロツキー主義（Trotskyism）はボルシェビキ革命の初期に宣言された労働者民主主義と国際主義の原則を堅持し、官僚化と一国社会主義の教義に強く反対している。<br>主要な人物：レフ・トロツキー、エルンスト・マンデル、陳独秀、王凡西、梁国雄",
+        "link": "https://en.wikipedia.org/wiki/Trotskyism"
+    },
+    {
+        "name": "レーニン主義",
+        "stats": {
+            "econ": 100,
+            "govt": 30,
+            "scty": 75
+        },
+        "desc": "レーニン主義（Leninism）は民主的中央集権主義の前衛党の樹立と、革命による資本主義の打倒を主張し、その目的を達成する為の一時的な強権行使の可能性を排除しない。<br>主要な人物：ウラジーミル・レーニン、チェ・ゲバラ、フィデル・カストロ、馬前卒（任沖昊）、汪暉",
+        "link": "https://en.wikipedia.org/wiki/Leninism"
+    },
+    {
+        "name": "マルクス・レーニン主義",
+        "stats": {
+            "econ": 95,
+            "govt": 5,
+            "scty": 30
+        },
+        "desc": "マルクス・レーニン主義（Marxism-Leninism）はスターリン政権下のソビエト連邦で発展した理論体系で、一党支配、厳しいイデオロギー・文化統制、一国社会主義の教義、高度に中央集権化された計画経済を特徴とする。<br>主要な人物：ヨシフ・スターリン、毛沢東、エンヴェル・ホッジャ",
+        "link": "https://en.wikipedia.org/wiki/Marxism%E2%80%93Leninism"
+    },
+    {
+        "name": "自由意志社会主義",
+        "stats": {
+            "econ": 85,
+            "govt": 90,
+            "scty": 85
+        },
+        "desc": "自由意志社会主義（Libertarian Socialism）は中央集権国家による社会や経済の統制に反対し、草の根レベルでの労働者の自治を主張する社会主義運動の学派である。<br>主要な人物：ミハイル・バクーニン、アンソニー・パンネコエク、ノーム・チョムスキー",
+        "link": "https://en.wikipedia.org/wiki/Libertarian_socialism"
+    },
+    {
+        "name": "民主社会主義",
+        "stats": {
+            "econ": 80,
+            "govt": 70,
+            "scty": 80
+        },
+        "desc": "民主社会主義（Democratic Socialism）は社会主義社会における民主主義の必要性を極権主義に反対する意味でも、過度の無政府主義的傾向を否定する意味でも強調している。<br>主要な人物：ユージン・デブス、ジョージ・オーウェル、ミロヴァン・ジラス、ジェレミー・コービン、史明",
+        "link": "https://en.wikipedia.org/wiki/Democratic_socialism"
+    },
+    {
+        "name": "社会民主主義",
+        "stats": {
+            "econ": 70,
+            "govt": 60,
+            "scty": 70
+        },
+        "desc": "社会民主主義（Social Democracy）は資本主義システムの段階的な改善を主張し、特定の目標ではなく社会の改善運動自体の意義を強調する。<br>主要な人物：エドゥワルド・ベルンシュタイン、ネルー、バーニー・サンダース、志位和夫",
+        "link": "https://en.wikipedia.org/wiki/Social_democracy"
+    },
+    {
+        "name": "左翼ポピュリズム",
+        "stats": {
+            "econ": 70,
+            "govt": 25,
+            "scty": 50
+        },
+        "desc": "左翼ポピュリズム（Left-Wing Populism）は既成エリートに反対し、社会正義のため、下層階級を守るため、既存の米国主導のグローバリゼーションと行き過ぎた資本主義に反対する。<br>主要な人物：ウゴ・チャベス、エボ・モラレス、温鉄軍、司馬南、孔慶東、趙皓陽",
+        "link": "https://en.wikipedia.org/wiki/Left-wing_populism"
+    },
+    {
+        "name": "民族ボルシェビズム",
+        "stats": {
+            "econ": 70,
+            "govt": 0,
+            "scty": 20
+        },
+        "desc": "民族ボルシェビズム（National Bolshevism）は民主主義、私的経済、国際主義に対して、ソ連型の政治・経済体制とファシズムに似た民族主義理念の結合を提唱した。<br>主要な人物：エルンスト・ニーキッシュ、エドゥワルド・リモノフ、アレクサンダドル・ドゥギン、至道学宮白雲先生（姚玉祥）",
+        "link": "https://en.wikipedia.org/wiki/National_Bolshevism"
+    },
+    {
+        "name": "無政府互助主義",
+        "stats": {
+            "econ": 65,
+            "govt": 100,
+            "scty": 60
+        },
+        "desc": "無政府互助主義（Mutualism）は資本主義的賃金労働に反対しつつ、自由市場の枠内で生産手段へのアクセスを社会化している。<br>主要な人物：ピエール＝ジョゼフ・プルードン",
+        "link": "https://en.wikipedia.org/wiki/Mutualism_(economic_theory)"
+    },
+    {
+        "name": "左翼自由意志主義",
+        "stats": {
+            "econ": 60,
+            "govt": 90,
+            "scty": 80
+        },
+        "desc": "左翼自由意志主義（Left-Libertarianism）は自由意志主義を堅持する一方で、資本主義的私有と賃金労働に抵抗し、特に天然資源の私的流用に反対した。<br>主要な人物：ヘンリー・ジョージ、デヴィッド・エラーマン、ヒル・スタイナー、フィリップ・ファン・パリス",
+        "link": "https://en.wikipedia.org/wiki/Left-libertarianism"
+    },
+    {
+        "name": "社会自由主義",
+        "stats": {
+            "econ": 55,
+            "govt": 60,
+            "scty": 70
+        },
+        "desc": "社会自由主義（Social Liberalism）は社会正義と経済的介入に好意的で、社会的・文化的進歩を特に重視する自由主義である。<br>主要な人物：ジョン・デューイ、ジョン・ロールズ、フランクリン・ルーズベルト、バラク・オバマ、ジャスティン・トルドー、盧武鉉、文在寅、蔡英文、陳純、秦暉、周保松、周濂",
+        "link": "https://en.wikipedia.org/wiki/Social_liberalism"
+    },
+    {
+        "name": "進歩保守主義",
+        "stats": {
+            "econ": 50,
+            "govt": 45,
+            "scty": 50
+        },
+        "desc": "進歩保守主義（Progressive Conservatism）進歩保守主義（Progressive Conservatism）は限定的な社会的セーフティネットと富の再分配、進歩的な社会変革を支持し、保守的な主張と進歩的な主張を併せ持つ。<br>主要な人物：ベンジャミン・ディズレーリ、アンジェラ・メルケル、デービッド・キャメロン、テレサ・メイ<br>",
+        "link": "https://en.wikipedia.org/wiki/Progressive_conservatism"
+    },
+    {
+        "name": "ナチズム",
+        "stats": {
+            "econ": 50,
+            "govt": 0,
+            "scty": 10
+        },
+        "desc": "ナチズム（National Socialism）は一般的なファシズムよりも人種差別を助長し国営経済を支持するファシズムに近い形態の一つである。<br>主要な人物：アドルフ・ヒトラー、ヨーゼフ・ゲッペルス、グレゴール・シュトラッサー",
+        "link": "https://en.wikipedia.org/wiki/Nazism"
+    },
+    {
+        "name": "ペロン主義",
+        "stats": {
+            "econ": 50,
+            "govt": 15,
+            "scty": 50
+        },
+        "desc": "ペロン主義（Peronism）、別名正義党主義（Justicialism）は政治主権、経済的独立、社会正義という3つのスローガンに要約される。 実践では、中央集権、民族の復興と民族の解放を主な要素としている。<br>主要な人物：フアン・ペロン、エクトル・ホセ・カンポラ",
+        "link": "https://en.wikipedia.org/wiki/Peronism"
+    },
+    {
+        "name": "自然法的無政府主義",
+        "stats": {
+            "econ": 40,
+            "govt": 100,
+            "scty": 60
+        },
+        "desc": "自然法的無政府主義（Individualist Anarchism）は社会は全体として契約から派生したものであり、個人はすべての契約から、すなわち社会から離脱する権利を持っていると主張している。<br>主要な人物：マックス・スタイナー、ライサンダー・スプーナー、ベンジャミン・タッカー",
+        "link": "https://en.wikipedia.org/wiki/Individualist_anarchism"
+    },
+    {
+        "name": "右翼ポピュリズム",
+        "stats": {
+            "econ": 40,
+            "govt": 25,
+            "scty": 25
+        },
+        "desc": "右翼ポピュリズム（Right-Wing Populism）は既成エリートに対抗し、貿易保護と反グローバル化を主張し、自国民優先主義と移民の厳格な管理を要求する。<br>主要な人物：ドナルド・トランプ、ジャイル・ボルソナロ、ボリス・ジョンソン、陳平、李子暘",
+        "link": "https://en.wikipedia.org/wiki/Right-wing_populism"
+    },
+    {
+        "name": "法西斯主義",
+        "stats": {
+            "econ": 35,
+            "govt": 5,
+            "scty": 15
+        },
+        "desc": "法西斯主義、あるいはファシズム（Fascism）はナショナリズムの極端な形態で、国際競争に直面している国家の利益を守るためには、国家が鉄拳で独裁体制を敷いて社会のあらゆる側面を支配しなければならないと強調する。<br>主要な人物：ベニート・ムッソリーニ、北一輝、ホセ・アントニオ、戴季陶、蒋介石",
+        "link": "https://en.wikipedia.org/wiki/Fascism"
+    },
+    {
+        "name": "資本主義的自由意志主義",
+        "stats": {
+            "econ": 30,
+            "govt": 90,
+            "scty": 60
+        },
+        "desc": "資本主義的自由意志主義（Libertarianism）は外交的孤立主義と完全な経済的自由放任主義を提唱し、功利主義よりも倫理的正当性を重視し、軍隊や警察といった国家の暴力に抵抗する。<br>主要な人物：フリードリヒ・ハイエク、レナード・リード、ロン・ポール、張維迎、朱海就、張五常",
+        "link": "https://en.wikipedia.org/wiki/Libertarianism"
+    },
+    {
+        "name": "新自由主義",
+        "stats": {
+            "econ": 30,
+            "govt": 55,
+            "scty": 60
+        },
+        "desc": "新自由主義（Neoliberalism）は大量の民営化と小さな政府を提唱する一方で、外交的には、自由主義の影響力を確保するために、貿易同盟を結び、敵に対して経済制裁を行う政府に依存している。<br>主要な人物：ビル・クリントン、資中筠、茅於軾、郎鹹平",
+        "link": "https://en.wikipedia.org/wiki/Neoliberalism"
+    },
+    {
+        "name": "保守主義",
+        "stats": {
+            "econ": 30,
+            "govt": 40,
+            "scty": 30
+        },
+        "desc": "保守主義（Conservatism）はフランス革命への反発から生まれ、伝統的な宗教的価値観や政治体制、富の分配を推進し、社会の安定と継続を目指している。<br>主要な人物：カルヴィン・クーリッジ、ウィンストン・チャーチル、シャルル・ド・ゴール、朴槿恵、安倍晋三、劉軍寧、蒋慶",
+        "link": "https://en.wikipedia.org/wiki/Conservatism"
+    },
+    {
+        "name": "新保守主義",
+        "stats": {
+            "econ": 20,
+            "govt": 30,
+            "scty": 25
+        },
+        "desc": "新保守主義（Neoconservatism）は民主主義的自由は国家の伝統に由来すると考え、文化的左派の過激な弾圧、伝統の擁護、愛国主義の構築、国際的な反民主主義勢力の制裁を提唱する。<br>主要な人物：ロナルド・レーガン、マーガレット・サッチャー、中曾根康弘、劉小楓",
+        "link": "https://en.wikipedia.org/wiki/Neoconservatism"
+    },
+    {
+        "name": "威権資本主義",
+        "stats": {
+            "econ": 20,
+            "govt": 15,
+            "scty": 30
+        },
+        "desc": "威権資本主義（Authoritarian Capitalism）が主張するのは個人の自由と政治的権利を制限する権威主義体制のもとでの資本主義市場経済であり、政府は通常、長期にわたって単一の政党に率いられる。<br>主要な人物：朴正熙、アウグスト・ピノチェト、ウラジーミル・プーチン、李光耀、鄭永年、李世默",
+        "link": "https://en.wikipedia.org/wiki/Authoritarian_capitalism"
+    },
+    {
+        "name": "古典的自由主義",
+        "stats": {
+            "econ": 15,
+            "govt": 65,
+            "scty": 60
+        },
+        "desc": "古典的自由主義（Classical Liberalism）は法の支配の下での市民的自由、特に経済的自由を重視し、自由放任の経済政策を提唱し、政府の機能はこれらの自由を守ることであると考えている。<br>主要な人物：ジョン・ロック、アダム・スミス",
+        "link": "https://en.wikipedia.org/wiki/Classical_liberalism"
+    },
+    {
+        "name": "無政府資本主義",
+        "stats": {
+            "econ": 0,
+            "govt": 95,
+            "scty": 60
+        },
+        "desc": "無政府資本主義（Anarcho-Capitalism）は私的所有と市場経済の自由な発展を保証するために、国家機構を廃止し、不可侵の原則に従って社会を組織することを提唱する。<br>主要な人物：マレー・ロスバード、ハンス・ヘルマン・ホッペ、サミュエル・エドワード・カンキン3世",
+        "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism"
+    },
+    {
+        "name": "小政府主義",
+        "stats": {
+            "econ": 0,
+            "govt": 85,
+            "scty": 60
+        },
+        "desc": "小政府主義（Minarchism）は政府の唯一の機能は、軍隊、警察、裁判所を通じて不可侵の原則を守ることであり、それによって侵略、窃盗、契約違反、詐欺、財産法の執行から市民を守ることであると主張する。<br>主要な人物：ロバート・ノージック、アイン・ランド",
+        "link": "https://en.wikipedia.org/wiki/Night-watchman_state"
+    }
+];
